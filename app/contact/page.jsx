2e12dada28c113b6bf7135373db1a43f932c3933
Maruslam <lan29138@gmail.com>
2024-10-11 16:05:37 +0700

    Update contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let`s work together.</h3>
               <p className="text-white/60">
-                fddsadasdafgdgfdgsdhjfgklsdjfasdfj{" "}
+              Hope you will give us the opportunity to work together. Thank you{" "}
               </p>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
